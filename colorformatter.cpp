f8519b69fc26895be580c74566ca8693e2278817
Fan, Yugang <yugang.fan@intel.com>
2017-08-14 14:24:20 +0800

    Init color formatter source code.

---
+++ colorformatter.cpp
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <getopt.h>
+
+int main(int argc, char *argv[]) {
+  printf("hellow world !\n");
+  return 0;
+}
